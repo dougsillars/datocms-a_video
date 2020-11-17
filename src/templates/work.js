5fda1fa895fa389f29b10685080200f7c43ff6f5
dougsillars <doug.sillars@gmail.com>
2020-11-17 15:04:30 +0000

    modifying page

--- src/templates/work.js
+++ src/templates/work.js
@@ -25,9 +25,9 @@ export default ({ data }) => (
             __html: data.datoCmsWork.descriptionNode.childMarkdownRemark.html,
           }}
         />
-        <div className="sheet__gallery">
-          <Img fluid={data.datoCmsWork.coverImage.fluid} />
-        </div>
+     //   <div className="sheet__gallery">
+     //     <Img fluid={data.datoCmsWork.coverImage.fluid} />
+     //   </div>
       </div>
     </article>
   </Layout>
