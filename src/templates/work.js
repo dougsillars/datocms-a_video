3bb36ad5d793c398e92d6fa3f7c94c82a1ef6bb2
dougsillars <doug.sillars@gmail.com>
2020-11-18 08:13:10 +0000

    modifying page

--- src/templates/work.js
+++ src/templates/work.js
@@ -26,13 +26,11 @@ export default ({ data }) => (
           }}
         />
 		<div class__name="sheet__video">
-		  <iframe src= {data.datoCmsWork.videoiframeurl} width=600px height =340px frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
-		  
+	  
 		  </div>
 		<div className="sheet__links">
 		  <a href={data.datoCmsWork.blogurl}>Blog Post</a> 
-		  <img src="https://www.datocms-assets.com/37728/1605610421-github-mark-32px.png"><a href={data.datoCmsWork.githuburl}>Github</a> 
-		  <a href={data.datoCmsWork.demourl}>{data.datoCmsWork.demourl}</a> 
+	
 		  </div>
       </div>
     </article>
