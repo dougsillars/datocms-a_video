19daea7dc1c82d8671794c14a230c51d3eb4aa8b
dougsillars <doug.sillars@gmail.com>
2020-11-18 08:01:09 +0000

    modifying page

--- src/templates/work.js
+++ src/templates/work.js
@@ -25,13 +25,15 @@ export default ({ data }) => (
             __html: data.datoCmsWork.descriptionNode.childMarkdownRemark.html,
           }}
         />
-		<div className="sheet__links">
-		  <h2><a href={data.datoCmsWork.blogURL}>Blog Post</a> </h2>
-		  <h2><a href={data.datoCmsWork.blogurl}>Blog Post</a> </h2>
+		<div class__name="sheet__video">
+		  <iframe src= {data.datoCmsWork.videoIframeurl}"#autoplay" width=600px height =340px frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
+		  
 		  </div>
-        <div className="sheet__gallery">
-          <Img fluid={data.datoCmsWork.coverImage.fluid} />
-        </div>
+		<div className="sheet__links">
+		  <a href={data.datoCmsWork.blogurl}>Blog Post</a> 
+		  <img src="https://www.datocms-assets.com/37728/1605610421-github-mark-32px.png"><a href={data.datoCmsWork.githuburl}>Github</a> 
+		  <a href={data.datoCmsWork.demourl}>{data.datoCmsWork.demourl}</a> 
+		  </div>
       </div>
     </article>
   </Layout>
