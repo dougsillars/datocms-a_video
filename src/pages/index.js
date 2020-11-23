6fcc0fe581e0ddb6414ebaecbc98b8941c215751
dougsillars <doug.sillars@gmail.com>
2020-11-23 21:39:14 +0000

    trying helpmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,20 +6,7 @@ import Layout from "../components/layout"
 import {Helmet} from "react-helmet";
 
 
-<Helmet>
-    <script>
-          var __nspid="isrtzw";
-          var __nsptags=[];
-          (function(w, d) { var x = function() {
-          var j=d.createElement("script");j.type="text/javascript";j.async=true;
-          j.src="http"+("https:"===d.location.protocol?"s://cs":"://c")+".ns1p.net/p.js?a="+__nspid;
-          d.body.appendChild(j); }
-          if(w.addEventListener) { w.addEventListener("load", x, false); }
-          else if(w.attachEvent) { w.attachEvent("onload", x); }
-          else { w.onload = x; }
-          }(window, document));
-    </script>
-</Helmet>
+
 const IndexPage = ({ data }) => (
 
   <Layout>
