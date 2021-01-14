f599439e63f25620c2788f963b333231543ba474
dougsillars <doug.sillars@gmail.com>
2021-01-14 14:43:11 +0000

    zap

--- src/templates/zap.js
+++ src/templates/zap.js
@@ -14,6 +14,7 @@ export default ({ data }) => (
         <h2 className="sheet__title"> <a href={data.datoCmsZap.demourl}>{data.datoCmsZap.demourl}</a></h2>
         <p className="sheet__lead">{data.datoCmsZap.excerpt}</p>
         <div className="sheet__from">
+          <Img fluid={data.datoCmsZap.fromLogo.fluid} />
             {data.datoCmsZap.from}
 
         </div>
