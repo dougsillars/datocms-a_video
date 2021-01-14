e578320528b84a0a380d11d1175addc5bc63b049
dougsillars <doug.sillars@gmail.com>
2021-01-14 13:14:30 +0000

    zap

--- src/templates/zap.js
+++ src/templates/zap.js
@@ -8,32 +8,32 @@ import Layout from "../components/layout"
 export default ({ data }) => (
   <Layout>
     <article className="sheet">
-      <HelmetDatoCms seo={data.datoCmsWork.seoMetaTags} />
+      <HelmetDatoCms seo={data.datoCmsZap.seoMetaTags} />
       <div className="sheet__inner">
-        <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
-        <h2 className="sheet__title"> <a href={data.datoCmsWork.demourl}>{data.datoCmsWork.demourl}</a></h2>
-        <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
+        <h1 className="sheet__title">{data.datoCmsZap.title}</h1>
+        <h2 className="sheet__title"> <a href={data.datoCmsZap.demourl}>{data.datoCmsZap.demourl}</a></h2>
+        <p className="sheet__lead">{data.datoCmsZap.excerpt}</p>
         <div className="sheet__slider">
           <Slider infinite={true} slidesToShow={2} arrows>
-            {data.datoCmsWork.gallery.map(({ fluid }) => (
-              <img alt={data.datoCmsWork.title} key={fluid.src} src={fluid.src} />
+            {data.datoCmsZap.gallery.map(({ fluid }) => (
+              <img alt={data.datoCmsZap.title} key={fluid.src} src={fluid.src} />
             ))}
           </Slider>
         </div>
         <div class__name="sheet__video">
-           <iframe src= {data.datoCmsWork.videoiframeurl} width='600px' height ='340px' frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
+           <iframe src= {data.datoCmsZap.videoiframeurl} width='600px' height ='340px' frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
 		    </div>
         <div
           className="sheet__body"
           dangerouslySetInnerHTML={{
-            __html: data.datoCmsWork.descriptionNode.childMarkdownRemark.html,
+            __html: data.datoCmsZap.descriptionNode.childMarkdownRemark.html,
           }}
         />
 
 		<div className="sheet__links">
-      <img src="https://www.datocms-assets.com/37728/1605691885-blogicon2.png?auto=compress%2Cformat" /><a href={data.datoCmsWork.blogurl}>Blog Post</a><br/>
-		  <img src="https://www.datocms-assets.com/37728/1605610549-github-mark-32px.png?auto=compress%2Cformat" /><a href={data.datoCmsWork.githuburl}>Github</a> <br/>
-		  <img src="https://www.datocms-assets.com/37728/1605691879-demoicon3.png?auto=compress%2Cformat" /><a href={data.datoCmsWork.demourl}>{data.datoCmsWork.demourl}</a><br/>
+      <img src="https://www.datocms-assets.com/37728/1605691885-blogicon2.png?auto=compress%2Cformat" /><a href={data.datoCmsZap.blogurl}>Blog Post</a><br/>
+		  <img src="https://www.datocms-assets.com/37728/1605610549-github-mark-32px.png?auto=compress%2Cformat" /><a href={data.datoCmsZap.githuburl}>Github</a> <br/>
+		  <img src="https://www.datocms-assets.com/37728/1605691879-demoicon3.png?auto=compress%2Cformat" /><a href={data.datoCmsZap.demourl}>{data.datoCmsZap.demourl}</a><br/>
 	
 		  </div>
       </div>
