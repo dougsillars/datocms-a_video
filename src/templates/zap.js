bf99cc73ec7e51fa66e8141c99292557b800acb9
dougsillars <doug.sillars@gmail.com>
2021-01-15 12:55:51 +0000

    zaps

--- src/templates/zap.js
+++ src/templates/zap.js
@@ -44,7 +44,7 @@ export default ({ data }) => (
         />
 
         <div className="sheet__links">
-          <a href={data.datoCmsZap.templateLink}>See the Zapier Template page</a><br/>
+          <a href={data.datoCmsZap.templateLink}>See the Zapier Template page</a><br/><br/>
 		      <a href={data.datoCmsZap.tryItlink}>Try it!</a> <br/>
 		      
 		    </div>
